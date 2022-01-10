ce43e38e796e2e68fc1f4ff5effd8c31214b9ee3
RyutoMotohashi <95869388+ryutomotohashi@users.noreply.github.com>
2022-01-10 11:52:46 +0000

    [FINISH] 2-7

---
+++ amounts.rb
@@ -0,0 +1,4 @@
+amounts = {"リンゴ"=>2,"イチゴ"=>5,"オレンシ"=>3}
+amounts.each do |fruit,amount|
+  puts "#{fruit}は、#{amount}個です。"
+end
--- lesson6.rb
+++ lesson6.rb
@@ -11,5 +11,5 @@ elsif total_price == 100
   puts "みかんを購入。所持金は0円"
 else
   puts "みかんを購入することができません"
-endgit 
+end
 
